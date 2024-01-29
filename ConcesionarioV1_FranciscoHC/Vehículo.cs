4e9776bdae22bf832ab0c314f7b3d2cda827396e
francisco <franciscoherrerachacon@gmail.com>
2024-01-29 12:37:18 +0100

    Clase Vehículo: Miembros privados, propiedades y constructores

--- ConcesionarioV1_FranciscoHC/Vehículo.cs
+++ ConcesionarioV1_FranciscoHC/Vehículo.cs
@@ -12,10 +12,44 @@ namespace ConcesionarioV1_FranciscoHC
 
 
         // Miembros Privados
+        private string _marca;
+        private string _modelo;
+
 
         // Constructores
+        public Vehículo(string marca, string modelo)
+        {
+            Marca = marca;
+            Modelo = modelo;
+        }
+
 
         // Propiedades
+        public string Marca
+        {
+            get
+            {
+                return _marca;
+            }
+            set
+            {
+                // TODO: Proporcionar seguridad y protección al miembro - _marca
+                _marca = value;
+            }
+        }
+
+        public string Modelo
+        {
+            get
+            {
+                return _modelo;
+            }
+            set
+            {
+                // TODO: Proporcionar seguridad y protección al miembro - _modelo
+                _modelo = value;
+            }
+        }
 
         // Métodos Públicos
 
