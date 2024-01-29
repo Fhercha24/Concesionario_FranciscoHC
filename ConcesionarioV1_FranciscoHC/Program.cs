6500a1f892dc5a3e54c865334bc7eeca06766ce2
francisco <franciscoherrerachacon@gmail.com>
2024-01-29 12:04:16 +0100

    Agregar archivos de proyecto.

---
+++ ConcesionarioV1_FranciscoHC/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace ConcesionarioV1_FranciscoHC
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Controlador principal de la aplicación
+
+
+            // Menú de acciones
+
+            // Procesamiento de las acciones
+
+        }
+    }
+}
